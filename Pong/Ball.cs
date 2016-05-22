ca2e12a251f4deaefd3e39a18beb0257f265a51d
Kevin Higgins <kev.higgins@gmail.com>
2016-05-22 17:29:55 -0500

    Fix ball direction on paddle impact to work more like pong

--- Pong/Ball.cs
+++ Pong/Ball.cs
@@ -1,18 +1,15 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System;
 
 namespace Pong
 {
-    class Ball
+    internal class Ball
     {
-        public Texture2D Texture;
-        public Vector2 Position;
         public Vector2 Direction;
-        public Boolean IsActive = false;
+        public bool IsActive = false;
+        public Vector2 Position;
         public int Speed = 5;
-
-        public Rectangle Bounds => new Rectangle((int)Position.X, (int)Position.Y, Texture.Width, Texture.Height);
+        public Texture2D Texture;
 
         public Ball(Texture2D texture, Vector2 position)
         {
@@ -20,6 +17,8 @@ namespace Pong
             Position = position;
             Direction = new Vector2(1, 0);
         }
+
+        public Rectangle Bounds => new Rectangle((int) Position.X, (int) Position.Y, Texture.Width, Texture.Height);
 
         public void UpdatePosition(Paddle leftPaddle, Paddle rightPaddle)
         {
@@ -34,13 +33,27 @@ namespace Pong
                 Position.X = leftPaddle.Position.X + leftPaddle.Texture.Width + 1;
             }
 
-            Position += Direction * Speed;
+            Position += Direction*Speed;
         }
 
         private Vector2 CalculateBallDirection(Paddle paddle, int neutralAngle, int multiplyOffset)
         {
-            var paddleCenterOffset = (paddle.Position.Y + (float)paddle.Texture.Height / 2 - Position.Y) * multiplyOffset;
-            var angle = (paddleCenterOffset * 2) + neutralAngle;
+            var halfPaddleWidth = (float) paddle.Texture.Height/2;
+            var paddleCenterY = paddle.Position.Y + halfPaddleWidth;
+            var ballCenterY = Position.Y + (float) Texture.Height/2;
+
+            var paddleCenterOffset = (paddleCenterY - ballCenterY)*multiplyOffset;
+
+            const int maxAngleOffset = 65;
+
+            // Percentage that the position of the impact was away from the center of the paddle. 100% is the very edge of the paddle.
+            var percentageOfMaxAngle = paddleCenterOffset / halfPaddleWidth;
+
+            // Adjust the ball's direction, so that the farther away from the center of the paddle it is, the larger the angle.
+            var addedAngle = percentageOfMaxAngle*maxAngleOffset;
+            
+
+            var angle = addedAngle + neutralAngle;
             return VectorMath.AngleToVector(angle);
         }
     }
